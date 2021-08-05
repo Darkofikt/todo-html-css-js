0d532c42f60b7bee207383157ced6052e4d82d66
Aleksandar Matevski <matevskial@gmail.com>
2021-08-05 22:45:21 +0200

    Implement add todo item in list

---
+++ index.js
@@ -0,0 +1,34 @@
+
+let ul = document.getElementById("task-list");
+
+// Add a task
+let addTaskBtn = document.getElementById("add-task-button");
+
+addTaskBtn.addEventListener("click", function(){
+
+    let ul = document.getElementById("task-list");
+    let inputString = document.getElementById("input-task");
+    let li = document.createElement("li");
+    let input = document.createElement("input");
+    let span = document.createElement("span");
+    let deleteBtn = document.createElement("button");
+
+    input.setAttribute("type", "checkbox");
+
+    deleteBtn.className = "delete-btn";
+    deleteBtn.innerHTML = "X";
+  
+    span.className = "task";
+    span.innerHTML = inputString.value;
+
+    if (inputString.value != ""){
+        li.append(input);
+        li.append(span);
+        li.append(deleteBtn);
+        ul.append(li);
+        inputString.value = "";
+    }
+});
+
+
+

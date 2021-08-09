2769d7b6f9f098317c14ae2f1c5cb67398bc9189
Darko <darkofikt@gmail.com>
2021-08-09 19:34:58 +0200

    Add Delete button functionality

--- index.js
+++ index.js
@@ -1,13 +1,9 @@
 
 let ul = document.getElementById("task-list");
-
-// Add a task
+let inputString = document.getElementById("input-task");
 let addTaskBtn = document.getElementById("add-task-button");
 
 addTaskBtn.addEventListener("click", function(){
-
-    let ul = document.getElementById("task-list");
-    let inputString = document.getElementById("input-task");
     let li = document.createElement("li");
     let input = document.createElement("input");
     let span = document.createElement("span");
@@ -21,6 +17,8 @@ addTaskBtn.addEventListener("click", function(){
     span.className = "task";
     span.innerHTML = inputString.value;
 
+    deleteBtn.addEventListener("click",deleteTodoElement);
+
     if (inputString.value != ""){
         li.append(input);
         li.append(span);
@@ -28,7 +26,38 @@ addTaskBtn.addEventListener("click", function(){
         ul.append(li);
         inputString.value = "";
     }
+
+
 });
+
+
+function addDeleteButton()
+{
+    let deletebuttons = document.getElementsByClassName("delete-btn")
+    for (let i=0;i<deletebuttons.length;i++)
+    {
+        let currentbutton = deletebuttons[i];
+        currentbutton.addEventListener("click",deleteTodoElement)
+    }
+}
+addDeleteButton();
+
+function deleteTodoElement(e)
+{   
+    console.log(typeof e);
+    let deleteBtn = e.target;
+    let parent = deleteBtn.parentElement;
+    ul.removeChild(parent);
+}
+
+
+
+
+
+
+
+
+
 
 
 

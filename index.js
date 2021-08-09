640be8ed71c6f511571f67ef78ab6488d407bc58
Darko <darkofikt@gmail.com>
2021-08-09 20:00:28 +0200

    Add new User Experience functionalities
    * Added refocus after adding a new task
    * Added the option to add tasks when Enter is pressed

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 
+
+
 let ul = document.getElementById("task-list");
 let inputString = document.getElementById("input-task");
 let addTaskBtn = document.getElementById("add-task-button");
 
-addTaskBtn.addEventListener("click", function(){
+addTaskBtn.addEventListener("click", addTask);
+document.addEventListener("keypress", addTask);
+
+function addDeleteButton()
+{
+    let deletebuttons = document.getElementsByClassName("delete-btn")
+    for (let i=0;i<deletebuttons.length;i++)
+    {
+        let currentbutton = deletebuttons[i];
+        currentbutton.addEventListener("click",deleteTodoElement)
+    }
+}
+addDeleteButton();
+
+function deleteTodoElement(e)
+{   
+    console.log(typeof e);
+    let deleteBtn = e.target;
+    let parent = deleteBtn.parentElement;
+    ul.removeChild(parent);
+}
+
+function addTask(e)
+{
+
+    if (e.code !== undefined && e.code  !== "Enter") return;
     let li = document.createElement("li");
     let input = document.createElement("input");
     let span = document.createElement("span");
@@ -26,28 +53,7 @@ addTaskBtn.addEventListener("click", function(){
         ul.append(li);
         inputString.value = "";
     }
-
-
-});
-
-
-function addDeleteButton()
-{
-    let deletebuttons = document.getElementsByClassName("delete-btn")
-    for (let i=0;i<deletebuttons.length;i++)
-    {
-        let currentbutton = deletebuttons[i];
-        currentbutton.addEventListener("click",deleteTodoElement)
-    }
-}
-addDeleteButton();
-
-function deleteTodoElement(e)
-{   
-    console.log(typeof e);
-    let deleteBtn = e.target;
-    let parent = deleteBtn.parentElement;
-    ul.removeChild(parent);
+    inputString.focus();
 }
 
 
